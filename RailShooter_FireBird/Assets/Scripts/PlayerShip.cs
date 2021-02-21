6e844f22b86fd975447dcba157c7c450ecd010c8
Fedor <dev.f1t1l@gmail.com>
2021-02-21 13:56:48 +0300

    no message

--- RailShooter_FireBird/Assets/Scripts/PlayerShip.cs
+++ RailShooter_FireBird/Assets/Scripts/PlayerShip.cs
@@ -17,7 +17,7 @@ public class PlayerShip : MonoBehaviour
 
     bool isControllEnabled = true;
     float pitch, yaw, roll, xThrow, xOffset, rawX, xMax, yThrow, yOffset, rawY, yMax;
-    // Update is called once per frame
+    
     void Update()
     {
         if (isControllEnabled)
