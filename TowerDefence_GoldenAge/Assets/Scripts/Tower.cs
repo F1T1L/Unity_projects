ffdc96888e1dafb5df6c2109ccfbdb7ddd0d1764
Fedor <dev.f1t1l@gmail.com>
2021-03-14 17:33:17 +0300

    It moves and dies!

--- TowerDefence_GoldenAge/Assets/Scripts/Tower.cs
+++ TowerDefence_GoldenAge/Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,37 @@ public class Tower : MonoBehaviour
 
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] ParticleSystem projectileParticle;
+    [SerializeField] float attackRange =40f;
 
     
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);        
+        if (targetEnemy)
+        {
+        objectToPan.LookAt(targetEnemy);
+        FireAtEnemy();
+        }
+        else{
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distance = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
+        if (distance <= attackRange)
+        {
+            Shoot(true);
+        }
+        else { 
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool value)
+    {
+        projectileParticle.gameObject.SetActive(value);
+        
     }
 }
