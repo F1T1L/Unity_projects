ffc7efc73838e7b16500e7ae3dc6c432529f6fd4
Fedor <dev.f1t1l@gmail.com>
2021-03-17 07:10:46 +0300

    Smooth MOve + Rotate Enemies
    4ре часа прошли не зря, оно двигается МЯГКО и наклоняется в  направлении движения.

--- TowerDefence_GoldenAge/Assets/Scripts/Enemy.cs
+++ TowerDefence_GoldenAge/Assets/Scripts/Enemy.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    enum Directions
+    {
+        Forward,
+        Back,
+        Left,
+        Right
+    };
     PathFinder pf;
     void Start()
     {
@@ -39,15 +46,71 @@ public class Enemy : MonoBehaviour
     {
         Vector3 startingPos = transform.position;
         Vector3 finalPos = waypoint.transform.position;
+        Quaternion startRot = transform.rotation;
+        Quaternion finalRot = waypoint.transform.rotation;   
+        
+        var heading = transform.position - waypoint.transform.position;
+        Directions direction = HeadingDirection(heading);
+        //print("Direction: " + direction);               
+        
         float elapsedTime = 0;
 
         while (elapsedTime < time)
         {
-            transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
+            transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));                      
+            transform.rotation = Quaternion.Lerp(startRot, SetRotationWhileMove(direction, finalRot,10f), (elapsedTime / time));
             elapsedTime += Time.deltaTime;
+            transform.rotation.SetLookRotation(waypoint.transform.position);
             yield return null;
         }
+      //  transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, smooth * Time.deltaTime);
     }
+
+    private Quaternion SetRotationWhileMove(Directions direction, Quaternion finalRot, float angle)
+    {
+        switch (direction)
+        {
+            case Directions.Forward:
+                finalRot = Quaternion.AngleAxis(angle, new Vector3(1f, 0f, 0f));
+                break;
+            case Directions.Back:
+                finalRot = Quaternion.AngleAxis(angle, new Vector3(-1f, 0f, 0f));
+                break;
+            case Directions.Left:
+                finalRot = Quaternion.AngleAxis(angle, new Vector3(0f, 0f, 1f));
+                break;
+            case Directions.Right:
+                finalRot = Quaternion.AngleAxis(angle, new Vector3(0f, 0f, -1f));
+                break;
+            default:
+                break;
+        }
+        return  finalRot;
+    }
+
+    private Directions HeadingDirection(Vector3 heading)
+    {             
+        if (heading.z <= -1f)
+        {            
+            return Directions.Forward;
+        }
+        if (heading.z >= 1f)
+        {            
+            return Directions.Back;
+        }
+        if (heading.x <= -1f)
+        {           
+            return Directions.Right;
+        }
+        if (heading.x >= 1f)
+        {            
+            return Directions.Left;
+        }
+        return Directions.Forward;
+    }
+
+   
+
     IEnumerator FollowPath(List<Waypoint> path)
     {
         foreach (Waypoint waypoint in path)
